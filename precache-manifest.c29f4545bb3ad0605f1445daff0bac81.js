96dc7901c32cf4809bb9071994f132e8b2067c84
gssmages <32328556+gssmages@users.noreply.github.com>
2020-01-28 19:48:55 +0530

    Updates

---
+++ precache-manifest.c29f4545bb3ad0605f1445daff0bac81.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceefea31700f6035014875147e1ade8e",
+    "url": "/gssmages/reactproject1/index.html"
+  },
+  {
+    "revision": "f6952f3817c83810600f",
+    "url": "/gssmages/reactproject1/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "836f54b40eb15cca2be1",
+    "url": "/gssmages/reactproject1/static/css/main.0e73bff0.chunk.css"
+  },
+  {
+    "revision": "f6952f3817c83810600f",
+    "url": "/gssmages/reactproject1/static/js/2.73d5c4e2.chunk.js"
+  },
+  {
+    "revision": "53b91fb6912e14c9689b5be87306d93a",
+    "url": "/gssmages/reactproject1/static/js/2.73d5c4e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "836f54b40eb15cca2be1",
+    "url": "/gssmages/reactproject1/static/js/main.069b89c5.chunk.js"
+  },
+  {
+    "revision": "e9ad370a510d273d5349",
+    "url": "/gssmages/reactproject1/static/js/runtime-main.5dc32349.js"
+  }
+]);
